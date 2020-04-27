84db86564858d3c2e7dffec6ae085fd0d56a2ac7
YerlanSharofidinov <180103077@stu.sdu.edu.kz>
2020-04-27 23:39:41 +0600

    first db project

---
+++ DB2/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace DB2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.eMPIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sTARTTIMEDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tERMSEASONDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tSCHEDULEBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataSet1 = new DB2.DataSet1();
+            this.tEACHERSCHEDULEBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tEACHER_SCHEDULETableAdapter = new DB2.DataSet1TableAdapters.TEACHER_SCHEDULETableAdapter();
+            this.t_SCHEDULETableAdapter = new DB2.DataSet1TableAdapters.T_SCHEDULETableAdapter();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tSCHEDULEBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tEACHERSCHEDULEBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(3, 4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(795, 445);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(787, 419);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(787, 419);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Search";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(87, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(262, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.eMPIDDataGridViewTextBoxColumn,
+            this.sTARTTIMEDataGridViewTextBoxColumn,
+            this.tERMSEASONDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.tSCHEDULEBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 77);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(343, 335);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // eMPIDDataGridViewTextBoxColumn
+            // 
+            this.eMPIDDataGridViewTextBoxColumn.DataPropertyName = "EMP_ID";
+            this.eMPIDDataGridViewTextBoxColumn.HeaderText = "EMP_ID";
+            this.eMPIDDataGridViewTextBoxColumn.Name = "eMPIDDataGridViewTextBoxColumn";
+            // 
+            // sTARTTIMEDataGridViewTextBoxColumn
+            // 
+            this.sTARTTIMEDataGridViewTextBoxColumn.DataPropertyName = "START_TIME";
+            this.sTARTTIMEDataGridViewTextBoxColumn.HeaderText = "START_TIME";
+            this.sTARTTIMEDataGridViewTextBoxColumn.Name = "sTARTTIMEDataGridViewTextBoxColumn";
+            // 
+            // tERMSEASONDataGridViewTextBoxColumn
+            // 
+            this.tERMSEASONDataGridViewTextBoxColumn.DataPropertyName = "TERM_SEASON";
+            this.tERMSEASONDataGridViewTextBoxColumn.HeaderText = "TERM_SEASON";
+            this.tERMSEASONDataGridViewTextBoxColumn.Name = "tERMSEASONDataGridViewTextBoxColumn";
+            // 
+            // tSCHEDULEBindingSource
+            // 
+            this.tSCHEDULEBindingSource.DataMember = "T_SCHEDULE";
+            this.tSCHEDULEBindingSource.DataSource = this.dataSet1;
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "DataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tEACHERSCHEDULEBindingSource
+            // 
+            this.tEACHERSCHEDULEBindingSource.DataMember = "TEACHER_SCHEDULE";
+            this.tEACHERSCHEDULEBindingSource.DataSource = this.dataSet1;
+            // 
+            // tEACHER_SCHEDULETableAdapter
+            // 
+            this.tEACHER_SCHEDULETableAdapter.ClearBeforeFill = true;
+            // 
+            // t_SCHEDULETableAdapter
+            // 
+            this.t_SCHEDULETableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tSCHEDULEBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tEACHERSCHEDULEBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private DataSet1 dataSet1;
+        private System.Windows.Forms.BindingSource tEACHERSCHEDULEBindingSource;
+        private DataSet1TableAdapters.TEACHER_SCHEDULETableAdapter tEACHER_SCHEDULETableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn eMPIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sTARTTIMEDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tERMSEASONDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource tSCHEDULEBindingSource;
+        private DataSet1TableAdapters.T_SCHEDULETableAdapter t_SCHEDULETableAdapter;
+    }
+}
+
---
+++ DB2/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DB2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataTable dt = new DataTable("t_schedule");
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["cn"].ConnectionString))
+                {
+                    if (cn.State == ConnectionState.Closed) 
+                        cn.Open();
+                    using (SqlDataAdapter da = new SqlDataAdapter("select * from t_schedule", cn))
+                    {
+                        da.Fill(dt);
+                        dataGridView1.DataSource = dt;
+                    }
+                }
+            }
+            catch (Exception ex){
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dataSet1.T_SCHEDULE' table. You can move, or remove it, as needed.
+            this.t_SCHEDULETableAdapter.Fill(this.dataSet1.T_SCHEDULE);
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13) {
+                DataView dv = dt.DefaultView;
+                dv.RowFilter = string.Format("select * from t_schedule where emp_id like '"
+                    +textBox1.Text+"%'", textBox1.Text);
+                dataGridView1.DataSource = dv.ToTable();
+            }
+        }
+    }
+}
